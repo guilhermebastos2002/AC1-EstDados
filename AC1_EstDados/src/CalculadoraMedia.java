93c3569ab545261dd391720845d6acc25ea74ca2
guilhermebastos2002 <126486967+guilhermebastos2002@users.noreply.github.com>
2024-02-23 09:21:26 -0300

    Delete AC1_EstDados directory

--- AC1_EstDados/src/CalculadoraMedia.java
+++
@@ -1,9 +0,0 @@
-public class CalculadoraMedia extends Paridade{
-    public static double calculaMedia(double... valores) {
-        double soma = 0.0;
-        for (double valor : valores) {
-            soma += valor;
-        }
-        return soma / valores.length;
-    }
-}
--- AC1_EstDados/src/Paridade.java
+++
@@ -1,9 +0,0 @@
-public class Paridade {
-    public static String verificaParidade(int numero){
-        if (numero % 2 == 0) {
-            return "Par";
-        } else {
-            return "Ímpar";
-        }
-    }
-}
